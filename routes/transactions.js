17eba511a7a8df770307c31a936b144b8cad543e
hardikrathod777 <137261879+hardikrathod777@users.noreply.github.com>
2024-09-06 13:47:01 +0530

    transactions.js

--- routes/transactions.js
+++ routes/transactions.js
@@ -2,41 +2,13 @@ const express = require('express');
 const router = express.Router();
 const transactionController = require('../controllers/transactionController');
 
+
 router.get('/', transactionController.getTransactions);
 router.get('/add', transactionController.getAddTransactionForm);
-
-// Route to handle form submission for adding a transaction
 router.post('/add', transactionController.addTransaction);
-router.post('/edit/:id', transactionController.editTransaction);
-router.get('/delete/:id', transactionController.deleteTransaction);
+router.get('/edit/:id', transactionController.getEditTransactionForm);
+router.post('/edit/:id', transactionController.updateTransaction);
+router.post('/delete/:id', transactionController.deleteTransaction);
+
 
 module.exports = router;
-// routes/transactions.js
-
-// const express = require('express');
-// const router = express.Router();
-// const Transaction = require('../models/transaction');
-
-// // Route to display all transactions
-// router.get('/', async (req, res, next) => {
-//   try {
-//     const transactions = await Transaction.find();
-//     res.render('index', { transactions });
-//   } catch (error) {
-//     next(error); // Pass errors to the error-handling middleware
-//   }
-// });
-
-// // Route to add a transaction
-// router.post('/add-transaction', async (req, res, next) => {
-//   try {
-//     const { type, category, amount, description, date } = req.body;
-//     const transaction = new Transaction({ type, category, amount, description, date });
-//     await transaction.save();
-//     res.redirect('/');
-//   } catch (error) {
-//     next(error); // Pass errors to the error-handling middleware
-//   }
-// });
-
-// module.exports = router;
